0719864cff0df4b72f048035cdf2fccf961fba89
bsaitkulov <bsaitkulov@gmail.com>
2023-04-06 01:10:58 +0400

    new transfer feature added

--- src/features/Transfer.java
+++ src/features/Transfer.java
@@ -3,6 +3,8 @@ package features;
 public class Transfer {
 
     public void transfer(String sender, String receiver){
-        System.out.println(receiver + " you receive 1000$ from: " + receiver);
+        System.out.println(receiver + " you receive 1000$ from: " + sender);
+
+        System.out.println(sender + " you sending 2000$ to: " + receiver);
     }
 }
