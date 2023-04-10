e390e4a638edcfe2c3cc47e72c61984b9c71aaa5
bsaitkulov <bsaitkulov@gmail.com>
2023-04-10 15:15:43 +0400

    added

--- src/features/Cities.java
+++ src/features/Cities.java
@@ -3,5 +3,6 @@ package features;
 public class Cities {
     public static void main(String[] args) {
         System.out.println("To visit Denpasar");
+        System.out.println("To visit Bali");
     }
 }
