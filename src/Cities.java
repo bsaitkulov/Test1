bd2c3905f7cb47188ab05f6630daeef0a4c47ab4
bsaitkulov <bsaitkulov@gmail.com>
2023-04-10 16:55:03 +0400

    Krabi added

--- src/Cities.java
+++ src/Cities.java
@@ -2,6 +2,7 @@ public class Cities {
     public static void main(String[] args) {
         System.out.println("To visit Denpasar");
         System.out.println("To visit Bali");
+        System.out.println("To visit Krabi");
 
     }
 }
